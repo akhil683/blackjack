5ce1c5bd8578f168342e63541aa2956be5cadc48
akhil683 <71344171+akhil683@users.noreply.github.com>
2023-03-21 01:37:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+let player = {
+    name: "Akhil",
+    chips: 100
+}
+
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardEl = document.getElementById("card-el");
+
+let cards = []
+let sum = 0;
+
+// whether or not player has blackjack
+let hasBlackJack = false;
+let isAlive = false;
+
+let message = "";
+
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
+
+function getRandomCard() {
+    
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
+    if( randomNumber === 1) {
+        return 11;
+    } else if  (randomNumber > 10) {
+        return 10;
+    } else {
+        return randomNumber;
+    }
+}
+
+ function startGame() {
+
+    isAlive = true;
+
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
+    renderGame();
+ }
+
+function renderGame() {
+
+    cardEl.textContent = "Cards: ";
+
+    for ( let i = 0; i < cards.length; i++ ) {
+        cardEl.textContent +=  cards[i] + " ";
+    }
+
+    sumEl.textContent = "Sum:" + sum;
+
+    if (sum <= 20) {
+
+        message ="Do you want to draw a new card?"
+
+    } else if (sum === 21) {
+        
+        message = "Wohoo! YOu've got Blackjack"
+        hasBlackJack = true;
+
+    } else {
+        
+        message = "You're out of the gmae!"
+        isAlive = false;
+
+    }
+
+    messageEl.textContent = message;
+}
+
+ function newCard() {
+
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard();
+        sum += card;
+    
+        cards.push(card)
+    
+        renderGame();
+    
+    }
+
+ }
